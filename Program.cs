77c02aefcff13dca023cfc6af82d19d73ab56395
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2022-03-17 14:38:53 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Create_Table_Project
+{
+    class Program
+    {
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=DESKTOP-NH7J05B;database=ProdiTI;Integrated Security=TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Coba (NIM char(12) not null primary key, Nama varchar(50), Alamat varchar(225), Jenis_Kelamin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel sukses dibuat!");
+                Console.ReadKey();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, sepertinya ada yang salah." + e);
+                Console.ReadKey();
+            }
+
+            finally
+            {
+                con.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+    }
+}
